89b883677b7962ccbf19bab17e7b97e921eedbb7
sergtco <sergtcorge@gmail.com>
2024-05-23 20:47:56 +0300

    Final?

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import json
 from re import error
 from flask import Flask, Response, request
 from songs_ranking import calculate_mfcc, rank_tracks_cosine
+from threading import Lock
 import sqlite3
 
-conn = sqlite3.connect('database.db')
-cursor = conn.cursor()
+mutex = Lock()
+
 
 def validate(json_data, fields):
     if all(field in json_data for field in fields):
@@ -14,6 +15,9 @@ def validate(json_data, fields):
 
 
 def mfcc():
+    mutex.acquire()
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
     """
     Gets json with {"path": string, "id": int} of song
     """
@@ -24,9 +28,11 @@ def mfcc():
 
     vec = list(map(float, calculate_mfcc(json_data["path"])))
     float_array_str = json.dumps(vec)
-    query = "INSERT INTO song_features (song_id, features) VALUES (?, ?)"
+    query = "INSERT INTO song_features (song_id, features) VALUES (?, ?) ON CONFLICT (song_id) DO NOTHING"
     cursor.execute(query, (json_data["id"], float_array_str))
     conn.commit()
+    conn.close()
+    mutex.release()
 
     resp = {
         "id": json_data["id"],
@@ -36,16 +42,36 @@ def mfcc():
 
 
 def radio():
+    mutex.acquire()
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
     """
     Gets json with {"target_id": int}
     """
     json_data = request.get_json()
     if not validate(json_data, ["target_id"]):
         return Response(status=400)
-    # rank_tracks_cosine(json_data["target_id"], ) TODO!!!
-    return "Unimplemented"
+    target_query = "SELECT features FROM song_features WHERE song_id=?"
+    res = cursor.execute(target_query, (json_data["target_id"],))
+    target_features = json.loads(res.fetchone()[0])
+    other_query = "select song_id, features from song_features where song_id != ?"
+    res = cursor.execute(other_query, (json_data["target_id"],))
+    other_features = {}
+    row = res.fetchone()
+    while row != None:
+        other_features[row[0]] = json.loads(row[1])
+        row = res.fetchone()
+
+    conn.commit()
+    conn.close()
+    mutex.release()
+    ranks = rank_tracks_cosine(target_features, other_features)
+    return ranks
+
 
 def init_db():
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
     query = """
     CREATE TABLE IF NOT EXISTS song_features (
         song_id INTEGER PRIMARY KEY,
@@ -55,10 +81,14 @@ def init_db():
     cursor.execute(query)
     conn.commit()
 
+
 def delete_song():
     """
     Gets json with {"target_id": int}
     """
+    mutex.acquire()
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
     json_data = request.get_json()
     if not validate(json_data, ["target_id"]):
         return Response(status=400)
@@ -66,6 +96,9 @@ def delete_song():
     query = "DELETE FROM song_features WHERE id = ?"
     cursor.execute(query, json_data["target_id"])
     conn.commit()
+    conn.close()
+    mutex.release()
+
 
 def main():
     init_db()
